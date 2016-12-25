28f1a702eeae5d3819c4880f899a7ef70f75f910
monde <monde@PANDORA>
2016-12-25 17:39:27 +0800

    Added Viewing of Lead Assigned to a certain user.

---
+++ RVL Management System/Forms/Frm_LeadAssigned.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace RVL_Management_System.Forms
+{
+    public partial class Frm_LeadAssigned : MetroForm
+    {
+        SqlConnection conn = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+
+        public Frm_LeadAssigned()
+        {
+            InitializeComponent();
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["connGlobal"].ToString();
+        }
+
+        public void loadData()
+        {
+            conn.Open();
+            cmd.Connection = conn;
+            string Show = "SELECT * FROM tblMarketing";
+            cmd.CommandText = Show;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridView.DataSource = dt;
+
+            conn.Close();
+
+            cmd.Parameters.Clear();
+        }
+
+        private void Frm_LeadAssigned_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
